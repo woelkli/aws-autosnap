07858ae3ce1e334da17f42263e2a29021401c556
Zach Himsel <zach@himsel.net>
2015-04-23 13:30:27 -0400

    Refactor/reformat logging system
    Makes the log a lot more readable, and fixes syntax issues.
    Also outputs log to both logfile and stdout.

--- autosnap.py
+++ autosnap.py
@@ -11,7 +11,6 @@ from boto.ec2.regioninfo import RegionInfo
 import boto.sns
 from datetime import datetime
 import time
-import sys
 import logging
 from config import config
 
@@ -29,12 +28,16 @@ count_errors = 0
 deletelist = []
 
 # Setup logging
-logging.basicConfig(filename=config['log_file'], level=logging.INFO)
-start_message = 'Started taking snapshots at %(date)s' % {
-    'date': datetime.today().strftime('%d-%m-%Y %H:%M:%S')
-}
-message += start_message + "\n\n"
-logging.info(start_message)
+logger = logging.getLogger('autosnap')
+logger.setLevel(logging.INFO)
+logfile = logging.FileHandler(config['log_file'])
+stdout = logging.StreamHandler()
+for handler in (logfile, stdout):
+    handler.setFormatter(logging.Formatter('%(message)s'))
+    logger.addHandler(handler)
+
+# Start log
+logging.info("\n\nStarted taking snapshots at %s", datetime.today().strftime('%d-%m-%Y %H:%M:%S'))
 
 # Get settings from config.py
 aws_access_key = config['aws_access_key']
@@ -66,11 +69,14 @@ else:
     # using roles
     if aws_access_key:
         conn = EC2Connection(aws_access_key, aws_secret_key, region=region)
+        logging.info("Authenticating with IAM access key: " + aws_access_key)
     else:
         conn = EC2Connection(region=region)
+        logging.info("Authenticating with IAM Role")
 
 # Connect to SNS
 if sns_arn:
+    logging.info("Detected SNS settings, using SNS")
     if proxyHost:
         # proxy:
         # using roles:
@@ -118,8 +124,13 @@ instances = conn.get_only_instances(filters={'tag:' + tag_name: tag_value})
 for instance in instances:
     try:
         keep_snapshots = int(instance.tags['autosnap_limit'])
+        logging.info("%s: Found instance, keeping %s snapshots (set by instance tag)",
+                     instance.id, keep_snapshots)
     except:
         keep_snapshots = config['keep_snapshots']
+        logging.info("%s: Found instance, keeping %s snapshots (set globally)",
+                     instance.id, keep_snapshots)
+
     # Get all the volumes attached to this instance
     volumes = conn.get_all_volumes(filters={
         'attachment.instance-id': instance.id})
@@ -129,9 +140,9 @@ for instance in instances:
         instance_name = "%s" % instance.id
     # Iterate through each volume attached to the selected instances
     for volume in volumes:
+        logging.info("%s:%s: Found volume, taking snapshot", instance.id, volume.id)
         try:
             count_total += 1
-            logging.info(volume)
             tags_volume = get_resource_tags(volume.id)
             # Detailed info for 'description' tag
             description = 'BACKUP: %(instance_name)s %(volume_id)s at %(date)s' % {
@@ -149,9 +160,12 @@ for instance in instances:
                                   {"snapshot_type": tag_name})
                 # Uses instance name for snapshot name
                 set_resource_tags(current_snapshot, {"Name": instance_name})
+                logging.info("%s:%s:%s: Snapshot created and tagged with \"%s\"",
+                             instance.id, volume.id, current_snapshot.id, tag_name)
                 total_creates += 1
             except Exception as e:
-                logging.error(e)
+                logging.error("%s:%s: Error while creating snapshot: %s",
+                              instance.id, volume.id, e)
                 pass
 
             snapshots = volume.snapshots()
@@ -160,13 +174,6 @@ for instance in instances:
                 tags_snapshot = get_resource_tags(snapshot.id)
                 if tag_name in tags_snapshot.values():
                     deletelist.append(snapshot)
-                else:
-                    logging.info('Skipping, not added to deletelist: '
-                                 + snapshot.id)
-
-            for snapshot in deletelist:
-                logging.info(snapshot)
-                logging.info(snapshot.start_time)
 
             def date_compare(snapshot1, snapshot2):
                 if snapshot1.start_time < snapshot2.start_time:
@@ -177,36 +184,27 @@ for instance in instances:
 
             deletelist.sort(date_compare)
             delta = len(deletelist) - keep_snapshots
-            for snapshot in range(delta):
-                del_message = (
-                    'Deleting snapshot '
-                    + deletelist[snapshot].description
-                    )
-                logging.info(del_message)
-                deletelist[snapshot].delete()
+            for deletesnap in range(delta):
+                snapshot = deletelist[deletesnap]
+                logging.info("%s:%s:%s: Deleting snapshot (%s)",
+                             instance.id, volume.id, snapshot.id, snapshot.start_time)
+                snapshot.delete()
                 total_deletes += 1
             time.sleep(3)
-        except:
-            logging.error('Error in processing volume with id: ' + volume.id)
-            errmsg += 'Error in processing volume with id: ' + volume.id
+
+        except Exception as e:
+            logging.error("%s:%s: Error processing volume: %s",
+                          instance.id, volume.id, e)
+            errmsg += "{0}:{1}: Error processing volume: {2}".format(instance.id, volume.id, e)
             count_errors += 1
         else:
             count_success += 1
 
 # Result message
-result = '\nFinished making snapshots at %(date)s with %(count_success)s \
-        snapshots of %(count_total)s possible.\n\n' % {
-    'date': datetime.today().strftime('%d-%m-%Y %H:%M:%S'),
-    'count_success': count_success,
-    'count_total': count_total
-}
-
-message += result
-message += "\nTotal snapshots created: " + str(total_creates)
-message += "\nTotal snapshots errors: " + str(count_errors)
-message += "\nTotal snapshots deleted: " + str(total_deletes) + "\n"
-
-logging.info('\n' + message + '\n')
+logging.info('Finished processing snapshots at %s ',
+             datetime.today().strftime('%d-%m-%Y %H:%M:%S'))
+logging.info("Total snapshots created/deleted/errors: %s/%s/%s",
+             str(total_creates), str(total_deletes), str(count_errors))
 
 # SNS reporting
 if sns_arn:
@@ -215,5 +213,3 @@ if sns_arn:
             sns_arn, 'Error in processing volumes: '
             + errmsg, 'Error with AWS Snapshot')
     sns.publish(sns_arn, message, 'Finished AWS snapshotting')
-
-logging.info(result)
