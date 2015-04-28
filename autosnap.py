c57c0b7b47d3965d445ad95b439922acd33e1fd4
Zach Himsel <zach@himsel.net>
2015-04-28 17:33:10 -0400

    Improve comments, readability, and organization

--- autosnap.py
+++ autosnap.py
@@ -3,6 +3,7 @@
 # (c) 2012/2014 E.M. van Nuil / Oblivion b.v.
 # Update 2015 by Zach Himsel
 
+# Load our dependent libraries
 from boto.ec2.connection import EC2Connection
 from boto.ec2.regioninfo import RegionInfo
 import boto.sns
@@ -12,17 +13,19 @@ import logging
 from config import config
 
 
-# Message to return result via SNS
+# Let's initialize some stuff to use later...
+# Init message to return result via SNS
 message = ""
 errmsg = ""
-
-# Counters
+# Init count variables
 total_creates = 0
 total_deletes = 0
 count_errors = 0
-
-# List with snapshots to delete
+count_success = 0
+count_total = 0
+# Init deletion list
 deletelist = []
+
 
 # Setup logging
 logging.basicConfig(level=logging.INFO,
@@ -30,13 +33,13 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%y-%m-%d %H:%M',
                     filename=config['log_file'],
                     filemode='a')
+# Set up log stream to mirror to stdout
 console = logging.StreamHandler(sys.stdout)
 console.setLevel(logging.INFO)
 logging.getLogger('').addHandler(console)
-
-
 # Start log
 logging.info("Initializing snapshot process")
+
 
 # Get settings from config.py
 ec2_region_name = config['ec2_region_name']
@@ -48,64 +51,48 @@ tag_name = config['tag_name']
 tag_value = config['tag_value']
 region = RegionInfo(name=ec2_region_name, endpoint=ec2_region_endpoint)
 
-count_success = 0
-count_total = 0
 
-# Connect to AWS (testing credentials)
-if proxyHost:
-    # proxy:
-    # using roles
-    try:
-        aws_access_key = config['aws_access_key']
-        aws_secret_key = config['aws_secret_key']
-        conn = EC2Connection(aws_access_key, aws_secret_key, region=region,
-                             proxy=proxyHost, proxy_port=proxyPort)
-    except:
-        conn = EC2Connection(region=region, proxy=proxyHost,
-                             proxy_port=proxyPort)
-else:
-    # non proxy:
-    # using roles
-    try:
-        aws_access_key = config['aws_access_key']
-        aws_secret_key = config['aws_secret_key']
-        conn = EC2Connection(aws_access_key, aws_secret_key, region=region)
-        logging.info("Authenticating with IAM access key: " + aws_access_key)
-    except:
-        conn = EC2Connection(region=region)
-        logging.info("Authenticating with IAM Role")
-
-# Connect to SNS
-if sns_arn:
-    logging.info("Detected SNS settings, using SNS")
+# Set up our AWS and SNS connection objects
+try:
+    # Try to load user-specified access keys (if they exist)
+    aws_access_key = config['aws_access_key']
+    aws_secret_key = config['aws_secret_key']
     if proxyHost:
-        # proxy:
-        # using roles:
-        if aws_access_key:
-            sns = boto.sns.connect_to_region(
-                ec2_region_name,
-                aws_access_key_id=aws_access_key,
-                aws_secret_access_key=aws_secret_key,
-                proxy=proxyHost, proxy_port=proxyPort)
-        else:
-            sns = boto.sns.connect_to_region(ec2_region_name, proxy=proxyHost,
-                                             proxy_port=proxyPort)
+        # Did the user specify proxy settings?
+        aws = EC2Connection(aws_access_key, aws_secret_key, region=region,
+                            proxy=proxyHost, proxy_port=proxyPort)
+        sns = boto.sns.connect_to_region(
+            ec2_region_name,
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_secret_key,
+            proxy=proxyHost, proxy_port=proxyPort)
     else:
-        # non proxy:
-        # using roles
-        if aws_access_key:
-            sns = boto.sns.connect_to_region(
-                ec2_region_name,
-                aws_access_key_id=aws_access_key,
-                aws_secret_access_key=aws_secret_key)
-        else:
-            sns = boto.sns.connect_to_region(ec2_region_name)
+        # No?
+        aws = EC2Connection(aws_access_key, aws_secret_key, region=region)
+        sns = boto.sns.connect_to_region(
+            ec2_region_name,
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_secret_key)
+    logging.info("Authenticating with IAM access key: " + aws_access_key)
+except:
+    # If they didn't, assume we're using an IAM role
+    if proxyHost:
+        # Did the user specify proxy settings?
+        aws = EC2Connection(region=region, proxy=proxyHost, proxy_port=proxyPort)
+        sns = boto.sns.connect_to_region(ec2_region_name, proxy=proxyHost,
+                                         proxy_port=proxyPort)
+    else:
+        # No?
+        aws = EC2Connection(region=region)
+        sns = boto.sns.connect_to_region(ec2_region_name)
+    logging.info("Authenticating with IAM Role")
 
 
+# Define function to get resource tags
 def get_resource_tags(resource_id):
     resource_tags = {}
     if resource_id:
-        tags = conn.get_all_tags({'resource-id': resource_id})
+        tags = aws.get_all_tags({'resource-id': resource_id})
         for tag in tags:
             # Tags starting with 'aws:' are reserved for internal use
             if not tag.name.startswith('aws:'):
@@ -113,49 +100,55 @@ def get_resource_tags(resource_id):
     return resource_tags
 
 
+# Define function to change resource tags
 def set_resource_tags(resource, tags):
     for tag_key, tag_value in list(tags.items()):
         if tag_key not in resource.tags or resource.tags[tag_key] != tag_value:
             resource.add_tag(tag_key, tag_value)
 
-# Get all the instances that match the tag criteria
-instances = conn.get_only_instances(filters={'tag:' + tag_name: tag_value})
 
-# Iterate through each instance in the list
+# Alright, let's start doing things
+# First, make a list of all the instances that match the tag criteria...
+instances = aws.get_only_instances(filters={'tag:' + tag_name: tag_value})
+
+# ...and do things for each one.
 for instance in instances:
     try:
+        # Check if the instance has a retention override tag
         keep_snapshots = int(instance.tags['autosnap_retention'])
         logging.info("%s: Found instance, keeping %s snapshots (set by instance tag)",
                      instance.id, keep_snapshots)
     except:
+        # Otherwise, set it to the global setting
         keep_snapshots = config['keep_snapshots']
         logging.info("%s: Found instance, keeping %s snapshots (set globally)",
                      instance.id, keep_snapshots)
 
-    # Get all the volumes attached to this instance
-    volumes = conn.get_all_volumes(filters={
+    # Make a list of all volumes attached to this instance
+    volumes = aws.get_all_volumes(filters={
         'attachment.instance-id': instance.id})
     if 'Name' in instance.tags:
         instance_name = "{0}".format(instance.tags['Name'])
     else:
         instance_name = "{0}".format(instance.id)
 
-    # Iterate through each volume attached to the selected instances
     for volume in volumes:
+        # Create a new snapshot for each volume
         logging.info("%s/%s: Found volume, taking snapshot", instance.id, volume.id)
         try:
             count_total += 1
+            # Get the volume's tags
             tags_volume = get_resource_tags(volume.id)
-            # Detailed info for 'description' tag
+            # Set the snapshot description
             description = "AUTOSNAP: {0} ({1}) at {2}".format(
                 instance_name,
                 volume.id,
                 datetime.today().strftime('%d-%m-%Y %H:%M:%S')
             )
             try:
-                # Create snapshot
+                # Create snapshot (and store the ID)
                 current_snapshot = volume.create_snapshot(description)
-                # Give snapshot the same tags from volume
+                # Give snapshot the same tags from its volume
                 set_resource_tags(current_snapshot, tags_volume)
                 # Give snapshot tag that indicates it's ours
                 set_resource_tags(current_snapshot, {"snapshot_type": tag_name})
@@ -169,28 +162,31 @@ for instance in instances:
                               instance.id, volume.id, e)
                 pass
 
-            snapshots = volume.snapshots()
-            deletelist = []
+            # Ok, now that the new snapshot has started, let's clean up the old ones
+            snapshots = volume.snapshots()  # Make a list of all the snapshots for this volume
+            deletelist = []  # make sure the delete list is blank!
             for snapshot in snapshots:
                 tags_snapshot = get_resource_tags(snapshot.id)
                 if tag_name in tags_snapshot.values():
                     deletelist.append(snapshot)
 
+            # Sort the delete list by snapshot age
             def date_compare(snapshot1, snapshot2):
                 if snapshot1.start_time < snapshot2.start_time:
                     return -1
                 elif snapshot1.start_time == snapshot2.start_time:
                     return 0
                 return 1
-
             deletelist.sort(date_compare)
+
+            # And trim off the latest X snapshots
             delta = len(deletelist) - keep_snapshots
             for deletesnap in range(delta):
                 snapshot = deletelist[deletesnap]
                 logging.info("%s/%s/%s: Deleting snapshot (%s)",
                              instance.id, volume.id, snapshot.id, snapshot.start_time)
-                snapshot.delete()
-                total_deletes += 1
+                snapshot.delete()  # Delete it
+                total_deletes += 1  # Increase our deletion counter
 
         except Exception as e:
             logging.error("%s/%s: Error processing volume: %s",
@@ -200,12 +196,12 @@ for instance in instances:
         else:
             count_success += 1
 
-# Result message
+# Finish up the log file...
 logging.info('Finished processing snapshots')
 logging.info("Total snapshots created/deleted/errors: %s/%s/%s",
              str(total_creates), str(total_deletes), str(count_errors))
 
-# SNS reporting
+# Report outcome to SNS (if configured)
 if sns_arn:
     if errmsg:
         sns.publish(
